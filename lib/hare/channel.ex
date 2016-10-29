6176dbf32cc5be94c8fd20cd77aea11eb7131a55
Jaime Cabot Campins <jcabot@gmail.com>
2016-10-29 20:08:52 +0200

    Channel.monitor, link and unlink

--- lib/hare/channel.ex
+++ lib/hare/channel.ex
@@ -5,4 +5,13 @@ defmodule Hare.Channel do
 
   def new(given, adapter),
     do: %Channel{given: given, adapter: adapter}
+
+  def monitor(%Channel{given: given, adapter: adapter}),
+    do: adapter.monitor_channel(given)
+
+  def link(%Channel{given: given, adapter: adapter}),
+    do: adapter.link_channel(given)
+
+  def unlink(%Channel{given: given, adapter: adapter}),
+    do: adapter.unlink_channel(given)
 end
---
+++ test/hare/channel_test.exs
@@ -0,0 +1,28 @@
+defmodule Hare.ChannelTest do
+  use ExUnit.Case, async: true
+
+  alias Hare.Channel
+  alias Hare.Adapter.Sandbox, as: Adapter
+
+  test "monitor/1, link/1 and unlink/1" do
+    {:ok, history} = Adapter.Backdoor.start_history
+    config = [history: history]
+
+    {:ok, given_conn} = Adapter.open_connection(config)
+    {:ok, given_chan} = Adapter.open_channel(given_conn)
+
+    channel = Channel.new(given_chan, Adapter)
+
+    ref = Channel.monitor(channel)
+    assert true = Channel.link(channel)
+    assert true = Channel.unlink(channel)
+
+    expected_events = [{:open_connection, [config],     {:ok, given_conn}},
+                       {:open_channel,    [given_conn], {:ok, given_chan}},
+                       {:monitor_channel, [given_chan], ref},
+                       {:link_channel,    [given_chan], true},
+                       {:unlink_channel,  [given_chan], true}]
+
+    assert expected_events == Adapter.Backdoor.events(history)
+  end
+end
