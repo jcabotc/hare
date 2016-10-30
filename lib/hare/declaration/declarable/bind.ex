8b729d0c916c1864cc8d0f3a234e70bcd1418d3a
Jaime Cabot Campins <jcabot@gmail.com>
2016-10-30 13:40:38 +0100

    common binding behaviour moved to a shared module Declarable.Shared.Binding

--- lib/hare/declaration/declarable/bind.ex
+++ lib/hare/declaration/declarable/bind.ex
@@ -1,38 +1,17 @@
 defmodule Hare.Declaration.Declarable.Bind do
   @behaviour Hare.Declaration.Declarable
 
-  @default_opts []
-
-  alias Hare.Declaration.Declarable.Helper
-  import Helper.Validations, only: [validate: 3, validate_keyword: 3]
+  alias Hare.Declaration.Declarable.Shared
 
   def validate(config) do
-    with :ok <- validate_queue(config),
-         :ok <- validate(config, :exchange, :binary),
-         :ok <- validate_keyword(config, :opts, required: false) do
-      :ok
-    end
+    Shared.Binding.validate(config)
   end
 
-  defp validate_queue(config) do
-    with {:error, {:not_present, _, _}} <- validate(config, :queue, :binary),
-         {:error, {:not_present, _, _}} <- validate(config, :queue_from_tag, :atom) do
-      {:error, {:not_present, [:queue, :queue_from_tag], config}}
-    end
+  def run(chan, config, tags) do
+    Shared.Binding.run(config, tags, &action(chan, &1, &2, &3))
   end
 
-  def run(%{given: given, adapter: adapter}, config, tags) do
-    with {:ok, queue} <- get_queue(config, tags) do
-      exchange = Keyword.get(config, :exchange)
-      opts     = Keyword.get(config, :opts, @default_opts)
-
-      adapter.bind(given, queue, exchange, opts)
-    end
-  end
-
-  defp get_queue(config, tags) do
-    with :error <- Keyword.fetch(config, :queue) do
-      Helper.Tag.get_through(config, tags, :queue_from_tag)
-    end
+  defp action(%{given: given, adapter: adapter}, queue, exchange, opts) do
+    adapter.bind(given, queue, exchange, opts)
   end
 end
