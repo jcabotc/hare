e930cbee7d9956d80644bc3e909d26ba3eea554d
Jaime Cabot Campins <jcabot@gmail.com>
2017-02-25 00:07:39 +0100

    Hare.Role

---
+++ test/hare/role_test.exs
@@ -0,0 +1,89 @@
+defmodule Hare.RoleTest do
+  use ExUnit.Case, async: true
+
+  alias Hare.Role
+
+  defmodule FrontLayer do
+    use Role.Layer
+
+    def channel(conn, _next, %{pid: pid} = state) do
+      {:ok, chan} = Hare.Core.Chan.open(conn)
+
+      send(pid, {:channel_front, chan})
+      {:ok, chan, state}
+    end
+
+    def handle_call(:foo, _from, _next, state),
+      do: {:reply, "FOO", state}
+    def handle_call(_anything, _from, next, state),
+      do: next.(state)
+
+    def handle_info(message, next, %{pid: pid} = state) do
+      send(pid, {:info_front, message})
+
+      with {:noreply, ^pid} <- next.(pid),
+        do: {:noreply, state}
+    end
+  end
+
+  defmodule BackLayer do
+    use Role.Layer
+
+    def init(_next, pid),
+      do: {:ok, %{pid: pid}}
+
+    def declare(chan, _next, %{pid: pid} = state) do
+      send(pid, {:declare_back, chan})
+      {:ok, state}
+    end
+
+    def handle_call(:bar, _from, _next, state),
+      do: {:reply, "BAR", state}
+
+    def handle_info(message, _next, pid) do
+      send(pid, {:info_back, message})
+      {:noreply, pid}
+    end
+
+    def terminate(_reason, _next, %{pid: pid}),
+      do: send(pid, :terminate_back)
+  end
+
+  alias Hare.Adapter.Sandbox, as: Adapter
+
+  test "everything" do
+    {:ok, history} = Adapter.Backdoor.start_history()
+
+    config = [adapter: Adapter,
+              backoff: [0, 1000],
+              config:  [history: history]]
+
+    {:ok, conn} = Hare.Conn.start_link(config)
+
+    test_pid = self()
+    layers   = [FrontLayer, BackLayer]
+
+    {:ok, role} = Role.start_link(conn, layers, test_pid)
+    assert_receive {:channel_front, chan_1}
+    assert_receive {:declare_back,  ^chan_1}
+
+    assert "FOO" == Role.call(role, :foo)
+    assert "BAR" == Role.call(role, :bar)
+
+    Adapter.Backdoor.crash(chan_1.given, :normal)
+    assert_receive {:channel_front, chan_2}
+    assert_receive {:declare_back,  ^chan_2}
+    assert chan_1 != chan_2
+
+    send(role, "baz")
+    assert_receive {:info_back, "baz"}
+    assert_receive {:info_front, "baz"}
+
+    ref = Process.monitor(role)
+    Process.unlink(role)
+
+    Role.cast(role, :qux)
+    reason = {:no_more_layers, :handle_cast, [:qux, %{pid: test_pid}]}
+    assert_receive {:DOWN, ^ref, _, _, ^reason}
+  end
+end
